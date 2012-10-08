af7268d48e3f92835d80beddc27916dcbefdae7e
santosb <bruno.santos@totaljobsgroup.com>
2012-10-08 17:27:22 +0100

    2 version

---
+++ FIfairy/_ReSharper.FIfairy/JbDecompilerCache/decompiler/FIfairyDomain-13cd/t/FIfairyDomain/IReleaseModel.cs
@@ -0,0 +1,13 @@
+﻿// Type: FIfairyDomain.IReleaseModel
+// Assembly: FIfairyDomain, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// Assembly location: C:\FI fairy\FIfairy\FIfairy\FIfairy\bin\FIfairyDomain.dll
+
+namespace FIfairyDomain
+{
+  public interface IReleaseModel
+  {
+    string ReleaseNumber { get; }
+
+    string TeamName { get; }
+  }
+}
